a7fdc6c955d271528b3fd3e87718e4ced535414f
Laurent Hirth <laurent.hirth.68@gmail.com>
2021-09-15 14:59:51 +0200

    wip

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # LaurentHirth_12_18082021
+
 [OC] Projet 12 - Développez un tableau de bord d'analytics avec React
+
+INstallation et prerequis
+
+composant dialog pour erreur + bouton retry
--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,29 +1,39 @@
+import { Activity } from '../models/Activity'
 const linkToBackend = 'http://localhost:3000/'
 
 export default class UserService {
+	async getUserInfo(userId) {
+		const response = await fetch(linkToBackend + 'user/' + userId)
+		const data = await response.json()
+		return data.data
+	}
 
-    async getUserInfo(userId) {
-        const response = await fetch(linkToBackend + 'user/' + userId)
-        const data = await response.json()
-        return data.data
-    }
+	async getUserActivity(userId) {
+		const response = await fetch(
+			linkToBackend + 'user/' + userId + '/activity'
+		)
+		const data = await response.json()
+		return data.data
+	}
 
-    async getUserActivity(userId) {
-        const response = await fetch(linkToBackend + 'user/' + userId + '/activity')
-        const data = await response.json()
-        return data.data
-    }
+	/**
+	 * @description
+	 * @param {*} userId
+	 * @returns
+	 */
+	async getUserAverageSession(userId) {
+		const response = await fetch(
+			linkToBackend + 'user/' + userId + '/average-sessions'
+		)
+		const data = await response.json()
+		return data.data
+	}
 
-    async getUserAverageSession(userId) {
-        const response = await fetch(linkToBackend + 'user/' + userId + '/average-sessions')
-        const data = await response.json()
-        return data.data
-    }
-
-    async getUserPerformance(userId) {
-        const response = await fetch(linkToBackend + 'user/' + userId + '/performance')
-        const data = await response.json()
-        return data.data
-    }
-
+	async getUserPerformance(userId) {
+		const response = await fetch(
+			linkToBackend + 'user/' + userId + '/performance'
+		)
+		const data = await response.json()
+		return data.data
+	}
 }
